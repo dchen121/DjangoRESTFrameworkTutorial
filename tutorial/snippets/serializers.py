395e56c557b13b41af50e54dfc20b5fd4111d337
Daniel Chen <chen.d__@hotmail.com>
2016-03-06 22:52:25 -0800

    created API views for snippet list and snippet detail, serves json responses

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
+
 class SnippetSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Snippet
 		fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
-		
+
 
 # class SnippetSerializer(serializers.Serializer):
 # 	pk = serializers.IntegerField(read_only=True)
