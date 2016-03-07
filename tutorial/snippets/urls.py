ae99a6c6eff7bf797ce6323b8095f317fbc030c7
Daniel Chen <chen.d__@hotmail.com>
2016-03-06 23:15:43 -0800

    refactored views, added format url suffixes

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
 	url(r'^snippets/$', views.snippet_list),
 	url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
+
+# Format suffixes to URLs to explicitly refer to a given format (eg. /snippets.json or /snippet/2.json)
+urlpatterns = format_suffix_patterns(urlpatterns)
